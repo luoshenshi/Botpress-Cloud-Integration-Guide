391356a4db1a46281917606c4370155ae5ce60a7
luoshenshi <pcnox7@gmail.com>
2023-12-05 13:20:57 +0530

    updated

--- index.js
+++ index.js
@@ -1,58 +1,10 @@
-const axios = require("axios");
-const request = require("request");
 const express = require("express");
 const app = express();
 
-const base_url = "https://api.botpress.cloud/v1/chat/messages/"; // The Main URL
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html"); // Serving index.html
+  res.sendFile(__dirname + "/public/settings.html");
 });
 
-app.use(express.static(__dirname + "/public")); // Serving static files from ./public folder
+app.use(express.static(__dirname + "/public"));
 
-// Messaging API
-
-app.get("/query", (req, res) => {
-  const options = {
-    url: base_url,
-    headers: {
-      "Content-type": "application/json",
-      Authorization: "...", // Your Authorization. Need help? Follow Documentation
-      "x-bot-id": "...", // Your bot id. Need help? Follow Documentation
-      "x-integration-id": "...", // Your x-integration-id. Need help? Follow Documentation
-    },
-    body: JSON.stringify({
-      userId: "...", // Your userId. Need help? Follow Documentation
-      conversationId: "...", // Your conversationId. Need help? Follow Documentation
-      type: "text",
-      payload: {
-        text: req.query.message, // Getting user message..
-      },
-      tags: {},
-    }),
-  };
-
-  // Making request using axios
-  axios
-    .post(options.url, options.body, { headers: options.headers })
-    .then((response) => {
-      console.log(response.data.message.payload.text);
-      setTimeout(() => {
-        request(options, (q, w, e) => {
-          const messages = JSON.parse(w.body).messages; // Messages List
-          if (messages.length > 0) {
-            res.json(messages[0]); // Sending the last message from Messages list ( Bot's Reply )
-          } else {
-            res.json({ error: "No messages found" }); // err
-          }
-        });
-      }, 3000);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.json({ error: "An error occurred" }); // err
-    });
-});
-
-app.listen(3000, console.log("running on port 3000"));
+app.listen(3100, console.log("running on port 3100"));
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,300 +1,152 @@
 const messages_container = document.querySelectorAll(".messages_container");
-
-const urlToMessages = "https://api.botpress.cloud/v1/chat/messages/"; // messages list url
-const urlToConversations = "https://api.botpress.cloud/v1/chat/conversations/"; // conversations list url
-const urlToUsers = "https://api.botpress.cloud/v1/chat/users/"; // users list url
-
-let options = {
-  method: "GET",
-  headers: {
-    "Content-type": "application/json",
-    Authorization: "...",
-    "x-bot-id": "...",
-  },
+const urls = {
+  messages: "https://api.botpress.cloud/v1/chat/messages/",
+  conversations: "https://api.botpress.cloud/v1/chat/conversations/",
+  users: "https://api.botpress.cloud/v1/chat/users/",
 };
 
-fetch(urlToMessages, options)
-  .then((response) => {
-    // Handle the response
-    if (response.ok) {
-      return response.json(); // Parse the JSON response
-    } else {
-      throw new Error("Network response was not ok.");
-    }
-  })
-  .then((data) => {
-    const countOFMessages = data.messages.length;
-    for (let i = 0; i < countOFMessages; i++) {
-      messages_container[0].innerHTML += `<div class="message">
-        <p style="display: none;">${data.messages[i].id}</p>
-        <p>${data.messages[i].payload.text}</p>
-        <button class="delete_messages">Delete</button>
-      </div>`;
-    }
-    // handling the delete_messages (button) event listener...
-    document.querySelectorAll(".delete_messages").forEach((button) => {
-      button.addEventListener("click", function () {
-        const url =
-          urlToMessages +
-          this.parentElement.children[0].innerText.replace("Id: ", "");
-        console.log(url);
-        let DelConvOptions = {
-          method: "DELETE",
-          headers: {
-            Accept: "*/*",
-            "Content-type": "application/json",
-            Authorization: "...", // bot token. Need any help? Check docs.
-            "x-bot-id": "...", // bot id. Need any help? Check docs.
-          },
-        };
-        fetch(url, DelConvOptions)
-          .then((response) => {
-            console.log(response);
-            if (response.ok) {
-              return response.json(); // Parse the JSON response
-            } else {
-              throw new Error("Network response was not ok.");
-            }
-          })
-          .then((data) => {
-            console.log(data);
-          });
-      });
-    });
-    // Handle the data from the response
-    // console.log(data); Log it If you once so that you can understand what going here!!
-  })
-  .catch((error) => {
-    // Handle any errors
-    console.error("Fetch error:", error);
-  });
+const headers = {
+  "Content-type": "application/json",
+  Authorization: "Bearer ...",
+  "x-bot-id": "...",
+};
 
-fetch(urlToConversations, options)
-  .then((response) => {
-    // Handle the response
-    if (response.ok) {
-      return response.json(); // Parse the JSON response
-    } else {
-      throw new Error("Network response was not ok.");
-    }
-  })
-  .then((data) => {
-    const countOFMessages = data.conversations.length;
-    for (let i = 0; i < countOFMessages; i++) {
-      messages_container[1].innerHTML += `<div class="message">
-        <p style="display: none;">Id: ${data.conversations[i].id}</p>
-        <p>Platform: ${data.conversations[i].integration}</p>
-        <button class="delete_conv">Delete</button>
-      </div>`;
-    }
-
-    // handling the delete_conv (button) event listener...
-    document.querySelectorAll(".delete_conv").forEach((button) => {
-      button.addEventListener("click", function () {
-        const url =
-          urlToConversations +
-          this.parentElement.children[0].innerText.replace("Id: ", "");
-        console.log(url);
-        let DelConvOptions = {
-          method: "DELETE",
-          headers: {
-            Accept: "*/*",
-            "Content-type": "application/json",
-            Authorization: "...", // bot token. Need any help? Check docs.
-            "x-bot-id": "...", // bot id. Need any help? Check docs.
-          },
-        };
-        fetch(url, DelConvOptions)
-          .then((response) => {
-            console.log(response);
-            if (response.ok) {
-              return response.json(); // Parse the JSON response
-            } else {
-              throw new Error("Network response was not ok.");
-            }
-          })
-          .then((data) => {
-            console.log(data);
-          });
-      });
-    });
-    // Handle the data from the response
-    // console.log(data); Log it If you once so that you can understand what going here!!
-  })
-  .catch((error) => {
-    // Handle any errors
-    console.error("Fetch error:", error);
-  });
-
-fetch(urlToUsers, options)
-  .then((response) => {
-    // Handle the response
-    if (response.ok) {
-      return response.json(); // Parse the JSON response
-    } else {
-      throw new Error("Network response was not ok.");
-    }
-  })
-  .then((data) => {
-    const countOFMessages = data.users.length;
-    for (let i = 0; i < countOFMessages; i++) {
-      if (data.users[i].name == undefined) {
-        messages_container[2].innerHTML += `<div class="message">
-        <p>Id: ${data.users[i].id}</p>
-        <h5>Name isn't present</h5>
-        <button class="delete_users">Delete</button>
-      </div>`;
+function fetchData(url, options, callback) {
+  fetch(url, options)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
       } else {
-        messages_container[2].innerHTML += `<div class="message">
-        <p style="display: none;">Id: ${data.users[i].id}</p>
-        <p>Name: ${data.users[i].name}</p>
-        <h5>Name is present</h5>
-        <button class="delete_users">Delete</button>
-      </div>`;
+        throw new Error("Network response was not ok.");
       }
-    }
+    })
+    .then((data) => {
+      callback(data);
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+}
 
-    // handling the delete_users (button) event listener...
-    document.querySelectorAll(".delete_users").forEach((button) => {
-      button.addEventListener("click", function () {
-        const url =
-          urlToUsers +
-          this.parentElement.children[0].innerText.replace("Id: ", "");
-        console.log(url);
-        let DelConvOptions = {
-          method: "DELETE",
-          headers: {
-            Accept: "*/*",
-            "Content-type": "application/json",
-            Authorization: "...", // bot token. Need any help? Check docs.
-            "x-bot-id": "...", // bot id. Need any help? Check docs.
-            Origin: "*",
-          },
-        };
-        fetch(url, DelConvOptions)
-          .then((response) => {
-            console.log(response);
-            if (response.ok) {
-              return response.json(); // Parse the JSON response
-            } else {
-              throw new Error("Network response was not ok.");
-            }
-          })
-          .then((data) => {
-            console.log(data);
-          });
+function createMessageElement(message, type) {
+  let messageElement = document.createElement("div");
+  messageElement.classList.add("message");
+  if (type == "users") {
+    if (message.name == "" || message.name == undefined) {
+      messageElement.innerHTML = `
+    <p style="display: none;">Id: ${message.id}</p>
+    <p>${type}: ${message.id}</p>
+    <button class="delete_${type}">Delete</button>
+  `;
+    } else {
+      messageElement.innerHTML = `
+    <p style="display: none;">Id: ${message.id}</p>
+    <p>${type}: ${message.name}</p>
+    <button class="delete_${type}">Delete</button>
+  `;
+    }
+  } else if (type == "conversations") {
+    messageElement.innerHTML = `
+    <p style="display: none;">Id: ${message.id}</p>
+    <p>${type}: ${message.integration}</p>
+    <button class="delete_${type}">Delete</button>
+  `;
+  } else {
+    messageElement.innerHTML = `
+    <p style="display: none;">Id: ${message.id}</p>
+    <p>${type}: ${message.payload.text}</p>
+    <button class="delete_${type}">Delete</button>
+  `;
+  }
+  return messageElement;
+}
+
+function deleteData(url, options) {
+  fetch(url, options)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network response was not ok.");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+function handleDeleteButtonClick(type, url) {
+  document.querySelectorAll(`.delete_${type}`).forEach((button) => {
+    button.addEventListener("click", function () {
+      const id = this.parentElement.children[0].innerText.replace("Id: ", "");
+      const deleteUrl = url + id;
+      deleteData(deleteUrl, {
+        method: "DELETE",
+        headers: headers,
       });
     });
-    // Handle the data from the response
-    // console.log(data); Log it If you once so that you can understand what going here!!
-  })
-  .catch((error) => {
-    // Handle any errors
-    console.error("Fetch error:", error);
   });
+}
+
+function displayMessages(data, container, type, url) {
+  data[type].forEach((item) => {
+    const messageElement = createMessageElement(item, type);
+    container.innerHTML += messageElement.outerHTML;
+  });
+  handleDeleteButtonClick(type, url);
+}
+
+function fetchAndDisplayData(url, options, container, type) {
+  fetchData(url, options, (data) => {
+    displayMessages(data, container, type, url);
+    console.log(type);
+  });
+}
+
+fetchAndDisplayData(
+  urls.messages,
+  { method: "GET", headers },
+  messages_container[0],
+  "messages"
+);
+fetchAndDisplayData(
+  urls.conversations,
+  { method: "GET", headers },
+  messages_container[1],
+  "conversations"
+);
+fetchAndDisplayData(
+  urls.users,
+  { method: "GET", headers },
+  messages_container[2],
+  "users"
+);
+
+function deleteAllData(url, type) {
+  fetchData(url, { method: "GET", headers }, (data) => {
+    if (data[type].length === 0) {
+      console.log(`No ${type} left to delete`);
+      return;
+    }
+    const deleteOptions = {
+      method: "DELETE",
+      headers: headers,
+    };
+    data[type].forEach((item) => {
+      const deleteUrl = url + item.id;
+      deleteData(deleteUrl, deleteOptions);
+    });
+  });
+}
 
 function delAllMsg() {
-  console.log("ok");
-  let option = {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json",
-      Authorization: "...",
-      "x-bot-id": "...",
-    },
-  };
-
-  fetch(urlToMessages, option)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.messages == "") {
-        console.log("No messages left to delete");
-        return;
-      }
-      option.method = "DELETE";
-      let times = data.messages.length;
-      let counter = 0;
-      let url = urlToMessages;
-      for (let i = 0; i < times; i++) {
-        url += data.messages[i].id;
-        fetch(url, option)
-          .then((response) => response.json())
-          .then((data) => {
-            counter++;
-            if (counter === times) {
-              console.log("All messages got deleted");
-            }
-          });
-      }
-    });
+  deleteAllData(urls.messages, "messages");
 }
+
 function delAllConv() {
-  console.log("ok");
-  let option = {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json",
-      Authorization: "...",
-      "x-bot-id": "...",
-    },
-  };
-
-  fetch(urlToConversations, option)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.conversations == "") {
-        console.log("No conversation left to delete");
-        return;
-      }
-      option.method = "DELETE";
-      let times = data.conversations.length;
-      let counter = 0;
-      let url = urlToConversations;
-      for (let i = 0; i < times; i++) {
-        url += data.conversations[i].id;
-        fetch(url, option)
-          .then((response) => response.json())
-          .then((data) => {
-            counter++;
-            if (counter === times) {
-              console.log("All conversations got deleted");
-            }
-          });
-      }
-    });
+  deleteAllData(urls.conversations, "conversations");
 }
-function delAllUsr() {
-  console.log("ok");
-  let option = {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json",
-      Authorization: "...",
-      "x-bot-id": "7f019b1e-fc93-49f1-a9bd-07aab5babbbb",
-    },
-  };
 
-  fetch(urlToUsers, option)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.users == "") {
-        console.log("No User left to delete");
-        return;
-      }
-      option.method = "DELETE";
-      let times = data.users.length;
-      let counter = 0;
-      let url = urlToUsers;
-      for (let i = 0; i < times; i++) {
-        url += data.users[i].id;
-        fetch(url, option)
-          .then((response) => response.json())
-          .then((data) => {
-            counter++;
-            if (counter === times) {
-              console.log("All Users got deleted");
-            }
-          });
-      }
-    });
+function delAllUsr() {
+  deleteAllData(urls.users, "users");
 }
